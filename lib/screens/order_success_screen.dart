a7883dd6db9ef96865cd645719d2fcba97a95daa
Gunadi-kripto <edward.stiawan28@gmail.com>
2025-10-05 17:04:38 +0700

    Update order_success_screen.dart
    ubah bahasa

--- lib/screens/order_success_screen.dart
+++ lib/screens/order_success_screen.dart
@@ -32,7 +32,7 @@ class OrderSuccessScreen extends StatelessWidget {
               
               const SizedBox(height: 30),
               const Text(
-                'Order Successful!',
+                'Pesanan Berhasil!',
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
